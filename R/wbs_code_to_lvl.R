0c5409352505fb403a44a2ef70e353396fa24237
daniel-r-germony <daniel.r.germony.civ@mail.mil>
2019-09-24 09:08:16 +0000

    trying to get wbs_code_to_lvl() to work

--- R/wbs_code_to_lvl.R
+++ R/wbs_code_to_lvl.R
@@ -8,22 +8,21 @@
 #' @return Returns an intiger assoicated with WBS Element Code (i.e., 0 for WBS
 #'   = "1.0", 1 for "1.1", 3 for "1.2.2.1")
 #' @export
-#'
-#' @examples
-wbs_code_to_lvl <- function(wbs_element_code, ...){
 
+wbs_code_to_lvl <- function(wbs_element_code, ...) {
   # Error prevention: Ensure the input is a char or factor, otherwise stop.
-  if(!is.character(wbs_element_code) &&
-              !is.factor(wbs_element_code)) {
-
+  if (!is.character(wbs_element_code) &&
+      !is.factor(wbs_element_code)) {
     stop(
       'This fuction only works with character or factor objects!\n',
-      'You have provided an object of class: ', class(wbs_element_code[1]))
+      'You have provided an object of class: ',
+      class(wbs_element_code[1])
+    )
   }
 
   # Error prevention: Ensure the input looks like a WBS Element Code.
-  if(!str_detect(string = wbs_element_code,
-                 pattern = wbs_code_regex)) {
+  if (!stringr::str_detect(string = wbs_element_code,
+                           pattern = wbs_code_regex)) {
     stop(
       'The object provided does not appear to be a WBS Element Code!\n',
       'WBS Element Codes start and end with a diget, include at least one\n',
@@ -33,34 +32,27 @@ wbs_code_to_lvl <- function(wbs_element_code, ...){
       '- 1.0\n',
       '- 1.1.3\n',
       '- 1.2.1.1.5.4.2'
-      )
+    )
   }
 
   # Starts with a diget, has at least one grouped period & diget, ends with a
   # diget.
   wbs_code_regex <- "^(\\d)(\\.\\d)+(\\d)*$"
 
+  # Edge Case: WBS Element Code "1.0" is an edge case that should return level
+  # "0" even though it otherwise meets the requirements as a level "1".
+  if (wbs_element_code == "1.0") {
+    wbs_lvl <- 0L
+    return(wbs_lvl)
 
-  if (str_detect(string = wbs_element_code,
-                 pattern = wbs_code_regex)) {
-    str_count(string = wbs_element_code,
-              pattern = "\\.")
   }
 
-  #return(wbs_lvl)
+  if (stringr::str_detect(string = wbs_element_code,
+                          pattern = wbs_code_regex)) {
+    wbs_lvl <- stringr::str_count(string = wbs_element_code,
+                                  pattern = "\\.")
+  }
+
+  return(wbs_lvl)
 
 }
-
-wbs_codes_col <- cdsr$reported_data %>% select(`WBS Element Code`)
-
-wbs_codes_col[[1]] %>% wbs_code_to_lvl()
-
-pass_single_char_wbs <- "1.1.2"
-fail_wbs_not_wbs_char <- "abc"
-fail_wbs_df_object <- iris %>% as_tibble()
-
-pass_single_char_wbs %>% wbs_code_to_lvl()
-fail_wbs_not_wbs_char %>% wbs_code_to_lvl()
-fail_wbs_df_object %>% wbs_code_to_lvl()
-
-
