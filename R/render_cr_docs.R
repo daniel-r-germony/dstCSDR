2371de8e37c196491a4c7253df15438d75b015e7
daniel-r-germony <daniel.r.germony.civ@mail.mil>
2019-10-10 09:10:56 +0000

    add documentation to reader_cr_docs

--- R/render_cr_docs.R
+++ R/render_cr_docs.R
@@ -1,7 +1,36 @@
+#' Create Cost Reporting Scope of Work from Template
+#'
+#' Imports a pre-formatted and filled in Excel file with info on the planned
+#' contract and system then generates a scope of work, CDRLs, and associated
+#' contract attachments from template documents.
+#'
+#' @param params_path Required. A full path to a filled-in
+#'   "cost_reporting_params.xlsx" file. If a pass is not passed to the function
+#'   call, a file selection box will be opened.
+#' @param output_dir Required. A directory path to where the user wants the
+#'   generated files to be placed. If a path is not passed by Windows users, a
+#'   pop-up box will appear. Non-Windows users must pass a directory path.
+#'
+#' @return Returns nothing to R but does put the following files at t \code{output_dir}:
+#'   \enumerate{
+#'     \item A Word document with cost reporting scope of work language.
+#'     \item A Word document with cost reporting CDRL language.
+#'     \item A partially complete CSDR Plan attachment.
+#'     \item A partially complete RDT attachment.
+#'     \item A partially complete LSPD Report attachment (if required).
+#'     \item A complete AUMC Report attachment (if required).
+#'   }
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' render_cr_docs(
+#'   params_path = here::here("inst", "cost_reporting_params.xlsx"),
+#'   output_dir = here::here()
+#' )
+#' }
 render_cr_docs <- function(params_path = file.choose(), output_dir = choose.dir()) {
-
-  # Add a message that non-Windows users must pass an output_dir. Windows users
-  # will be provided with a choose.dir popup box.
 
   params_from_excel <-
     readxl::read_excel(path = params_path,
@@ -12,7 +41,8 @@ render_cr_docs <- function(params_path = file.choose(), output_dir = choose.dir(
   rmarkdown::render(
     input = here::here("inst", "extdata", "cost_reporting_sow_template.Rmd"),
     #output = "word_document",
-    output_file = "cost_reporting_sow.docx",
+    output_file = paste0(
+      Sys.Date(), "_", params_from_excel$system_name_abbr, "_cost-reporting-sow.docx"),
     output_dir = if (exists("output_dir")) output_dir else here::here(),
     params = params_from_excel
   )
