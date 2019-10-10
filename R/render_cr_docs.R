5d62b95b9cb54b596d2e09c658029e63c692c34b
daniel-r-germony <daniel.r.germony.civ@mail.mil>
2019-10-10 23:53:12 +0000

    documentation updates to render_cr_docs

--- R/render_cr_docs.R
+++ R/render_cr_docs.R
@@ -1,25 +1,30 @@
-#' Create Cost Reporting Scope of Work from Template
+#' Create Cost Reporting SOW, CDRLs, Attachments with Params
 #'
 #' Imports a pre-formatted and filled in Excel file with info on the planned
 #' contract and system then generates a scope of work, CDRLs, and associated
 #' contract attachments from template documents.
 #'
 #' @param params_path Required. A full path to a filled-in
-#'   "cost_reporting_params.xlsx" file. If a pass is not passed to the function
-#'   call, a file selection box will be opened.
+#'   "cost_reporting_params.xlsx" file. If a path is not provided to the
+#'   function call, a file selection box will be opened.
 #' @param output_dir Required. A directory path to where the user wants the
 #'   generated files to be placed. If a path is not passed by Windows users, a
 #'   pop-up box will appear. Non-Windows users must pass a directory path.
 #'
-#' @return Returns nothing to R but does put the following files at t \code{output_dir}:
-#'   \enumerate{
-#'     \item A Word document with cost reporting scope of work language.
-#'     \item A Word document with cost reporting CDRL language.
-#'     \item A partially complete CSDR Plan attachment.
-#'     \item A partially complete RDT attachment.
-#'     \item A partially complete LSPD Report attachment (if required).
-#'     \item A complete AUMC Report attachment (if required).
-#'   }
+#' @return Returns nothing to R but does put the following files at the
+#'   \code{output_dir}: \enumerate{ \item A Word document with cost reporting
+#'   scope of work language, filled in with the param values from the
+#'   "cost_reporting_params.xlsx" file. \item A Word document with cost
+#'   reporting CDRLs, filled in with the param values from the
+#'   "cost_reporting_params.xlsx" file. \item A partially complete CSDR Plan
+#'   attachment, filled in with the param values from the
+#'   "cost_reporting_params.xlsx" file. \item A partially complete RDT
+#'   attachment, filled in with the param values from the
+#'   "cost_reporting_params.xlsx" file. \item A partially complete LSPD Report
+#'   attachment (if required), filled in with the param values from the
+#'   "cost_reporting_params.xlsx" file. \item A complete AUMC Report attachment
+#'   (if required), filled in with the param values from the
+#'   "cost_reporting_params.xlsx" file. }
 #'
 #' @export
 #'
@@ -28,8 +33,7 @@
 #' render_cr_docs(
 #'   params_path = here::here("inst", "cost_reporting_params.xlsx"),
 #'   output_dir = here::here()
-#' )
-#' }
+#' )}
 render_cr_docs <- function(params_path = file.choose(), output_dir = choose.dir()) {
 
   params_from_excel <-
