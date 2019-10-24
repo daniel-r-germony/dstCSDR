7ebf92c82fb6d3e80ec0b5e1ca6cbd29cf5e7767
daniel-r-germony <daniel.r.germony.civ@mail.mil>
2019-10-24 08:54:02 +0000

    updates to render_cr_docs
    replaced "files_to_render" to "combined_cdrls" since the only real difference is going to be if each CDRL is it its own or multiple files.
    
    started coding up the CDRL rendering

--- R/render_cr_docs.R
+++ R/render_cr_docs.R
@@ -1,8 +1,8 @@
 #' Create Cost Reporting SOW, CDRLs, Attachments with Params
 #'
 #' Imports a pre-formatted and filled in Excel file with info on the planned
-#' contract and system then generates a scope of work, CDRLs, and associated
-#' contract attachments from template documents.
+#' contract and system then generates a scope of work, CDRLs, and  (comming
+#' soon) associated contract attachments from template documents.
 #'
 #' @param params_path Required. A full path to a filled-in
 #'   "cost_reporting_params.xlsx" file. If a path is not provided to the
@@ -11,39 +11,31 @@
 #'   generated files to be placed. If using Windows, a pop-up box will ppear to
 #'   specify an output directory. Non-Windows users must pass a directory path
 #'   in the function call.
-#' @param files_to_render Optional. Specify which files are rendered and how
-#'   they are returned. Options:
-#'   \describe{
-#'     \item{\code{"default"}}{The default value and will render and return the
-#'     following:
-#'       \enumerate{
-#'         \item SOW: All SOW language in one ".docx" file.
-#'         \item CDRLs: All cost reporting CDRLs in one ".docx" file.
-#'         }}
-#'     \item{\code{"cade_ready"}}{Returns the same files as \code{"default"}
-#'     but splits the CDRL document into multiple files (each CSDR CDRL gets
-#'     its own file and each non-CSDR CDRL gets lumped into one file) for
-#'     easier uploading into the CADE's Program Planning Module.
-#'     }}
+#' @param combine_cdrls Optional. Should all CDRLs be written into one .docx or
+#'   one .docx per CDRL. Options:
+#'     \describe{
+#'       \item{\code{"TRUE"}}{The CDRLs will be included in one Word file. The
+#'         default.}
+#'       \item{\code{"FALSE"}}{Each CDRL is written to its own Word file.}}
 #'
 #' @return Returns nothing to R but does puts the following files at the
-#'   \code{output_dir}, dependent on what is passed to the
-#'   \code{files_to_render} paramater:
+#'   \code{output_dir}:
 #'     \enumerate{
-#'       \item A Word document with cost reporting scope of work language,
+#'       \item A Word file (.docx) with cost reporting scope of work language,
 #'       filled in with the param values from the "cost_reporting_params.xlsx"
 #'       file.
-#'       \item A Word document or documents with cost reporting CDRLs, filled
-#'       in with the param values from the "cost_reporting_params.xlsx" file.
-#'       \item A partially complete CSDR Plan attachment, filled in with the
-#'       param  values from the "cost_reporting_params.xlsx" file.
-#'       \item A partially complete RDT attachment, filled in with the param
-#'       values from the "cost_reporting_params.xlsx" file.
-#'       \item A partially complete LSPD Report attachment (if required),
+#'       \item A Word file (.docx) or Word files with cost reporting CDRLs,
 #'       filled in with the param values from the "cost_reporting_params.xlsx"
 #'       file.
-#'       \item A complete AUMC Report attachment (if required), filled in with
-#'       the param values from the "cost_reporting_params.xlsx" file.
+#       \item A partially complete CSDR Plan attachment, filled in with the
+#       param  values from the "cost_reporting_params.xlsx" file.
+#       \item A partially complete RDT attachment, filled in with the param
+#       values from the "cost_reporting_params.xlsx" file.
+#       \item A partially complete LSPD Report attachment (if required),
+#       filled in with the param values from the "cost_reporting_params.xlsx"
+#       file.
+#       \item A complete AUMC Report attachment (if required), filled in with
+#       the param values from the "cost_reporting_params.xlsx" file.
 #'       }
 #'
 #' @export
@@ -62,13 +54,15 @@
 #'          }
 render_cr_docs <- function(params_path = file.choose(),
                            output_dir = choose.dir(),
-                           files_to_render = "default") {
+                           combine_cdrls = TRUE) {
 
-  # Error prevention: Check that `files_to_render` was an allowable value.
-  if (!(files_to_render == "default" || files_to_render == "cade_ready")) {
+  # SETUP =====================================================================
+
+  # Error prevention: Check that `combine_cdrls` was an allowable value.
+  if (!(combine_cdrls == TRUE || combine_cdrls == FALSE)) {
     stop(glue::glue(
-        "\n\n The `files_to_render` paramater must be either \"default\" or \"cade_ready\". \n
-        The function was passsed the value, \"{files_to_render}\"")
+        "\n\n The `combine_cdrls` paramater must be either \"TRUE\" or \"FALSE\". \n
+        The function was passsed the value, \"{combine_cdrls}\"")
     )
   }
 
@@ -82,11 +76,34 @@ render_cr_docs <- function(params_path = file.choose(),
 
   # Source the .cdrl_block_info.R script to read some of the basic CDRL info
   # into R (which will be needed in the render step).
-
   source(here::here("R",".cdrl_block_info.R"),
          local = TRUE) # `local = TRUE` lets this script access the `params_from_excel`
 
-  # Reader the SOW document docx. =============================================
+  # This is a function to help render each CDRL into its own Word document.
+  .render_cr_cdrl_doc <- function(params_path = params_path,
+                                  output_dir = output_dir,
+                                  rmd_cdrl){
+    rmarkdown::render(
+      input = system.file("extdata",
+                          rmd_cdrl,
+                          package = "dstCSDR"),
+      output_file = paste0(Sys.Date(),
+                           "_",
+                           params_from_excel$system_name_abbr,
+                           "_CDRL_",
+                           cdrl_title_abbr,
+                           ".docx"),
+      output_dir = if (exists("output_dir"))
+        output_dir
+      else
+        here::here(),
+      params = params_from_excel
+    )
+  }
+
+  # EXECUTION =================================================================
+
+  # Render the SOW document docx. =============================================
   rmarkdown::render(
     input = system.file("extdata",
                         "cost_reporting_sow_template.Rmd",
@@ -102,22 +119,21 @@ render_cr_docs <- function(params_path = file.choose(),
     params = params_from_excel
   )
 
-  # Default
-  if (files_to_render == "default") {
-    rmarkdown::render(
-      input = system.file("extdata",
-                          "cost_reporting_sow_template.Rmd",
-                          package = "dstCSDR"),
-      output_file = paste0(Sys.Date(),
-                           "_",
-                           params_from_excel$system_name_abbr,
-                           "_cost-reporting-sow.docx"),
-      output_dir = if (exists("output_dir"))
-        output_dir
-      else
-        here::here(),
-      params = params_from_excel
-    )
+  # Render each CDRL into its own Word document.
+  if (params_from_excel$include_cdrl_xyz == TRUE) # If the user marked the CDRL is needed
+    .render_cr_cdrl_doc(params_path, output_dir, "cdrl.Rmd") # Replace "cdrl.Rmd"
+  if (params_from_excel$include_cdrl_xyz == TRUE)# If the user marked the CDRL is needed
+    .render_cr_cdrl_doc(params_path, output_dir, "cdrl.Rmd") # Replace "cdrl.Rmd"
+  if (params_from_excel$include_cdrl_xyz == TRUE)# If the user marked the CDRL is needed
+    .render_cr_cdrl_doc(params_path, output_dir, "cdrl.Rmd") # Replace "cdrl.Rmd"
+  #...
+  if (params_from_excel$include_cdrl_xyz == TRUE)# If the user marked the CDRL is needed
+  .render_cr_cdrl_doc(params_path, output_dir, "cdrl.Rmd") # Replace "cdrl.Rmd"
+
+  # Render all CDRLs into one Word document ===================================
+  if (combine_cdrls == TRUE) {
+  # Add code that stitches each CDRL into one word doc
+  # Also delete each of the individual CDRL files
   }
 
 }
